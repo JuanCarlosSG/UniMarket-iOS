c237c57e7a0371c27adc67664997b075974cc90e
JuanCarlosSG <jcsagu00@gmail.com>
2022-05-08 20:44:42 -0500

    Initial Commit

---
+++ Unimarket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unimarket
+//
+//  Created by Juan Carlos Sánchez Gutiérrez on 08/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unimarket/UnimarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnimarketApp.swift
+//  Unimarket
+//
+//  Created by Juan Carlos Sánchez Gutiérrez on 08/05/22.
+//
+
+import SwiftUI
+
+@main
+struct UnimarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
